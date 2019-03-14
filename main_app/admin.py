db7e4ca0725390983328b391078cf4fc35bfc236
Son Truong <sntruong12@gmail.com>
2019-03-13 23:22:32 -0500

    add Marketplace model to admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Shoe, Cleaning
+from .models import Shoe, Cleaning, Marketplace
 # Register your models here.
 admin.site.register(Shoe)
 admin.site.register(Cleaning)
+admin.site.register(Marketplace)
