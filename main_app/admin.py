a9c4344b53188d2fc6c25ac674377c4e95387f44
Son Truong <sntruong12@gmail.com>
2019-03-15 16:25:36 -0500

    add function for adding photos

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Shoe, Cleaning, Marketplace
+from .models import Shoe, Cleaning, Marketplace, Photo
 # Register your models here.
 admin.site.register(Shoe)
 admin.site.register(Cleaning)
 admin.site.register(Marketplace)
+admin.site.register(Photo)
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Shoe, Marketplace
+from .models import Shoe, Marketplace, Photo
 from .forms import CleaningForm
 import datetime
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3-us-east-2.amazonaws.com/'
+BUCKET = 'shoecollector9000'
 
 # Define the home view
 def home(request):
@@ -45,6 +50,25 @@ def add_cleaning(request, shoe_id):
     new_cleaning.shoe_id = shoe_id
     new_cleaning.save()
   
+  return redirect('detail', shoe_id=shoe_id)
+
+def add_photo(request, shoe_id):
+	# photo-file was the "name" attribute on the <input type="file">
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" for S3 / needs image file extension too
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    # just in case something goes wrong
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      # build the full url string
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      # we can assign to shoe_id or shoe if you have a shoe object
+      photo = Photo(url=url, shoe_id=shoe_id)
+      photo.save()
+    except:
+      print('An error occurred uploading file to S3')
   return redirect('detail', shoe_id=shoe_id)
 
 class ShoeCreate(CreateView):
